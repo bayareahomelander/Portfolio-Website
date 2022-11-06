f058609234f90fcedeacb0cf7aba18fdf6392fda
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-11-06 15:13:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass('sticky');
+        }else{
+            $('.navbar').removeClass('sticky');
+        }
+
+        if(this.scroll > 50) {
+            $('.scroll').addClass('show');
+        }else{
+            $('.scroll').removeClass('show');
+        }
+    });
+
+    $('.scroll').click(function(){
+        $('html').animate({scrollTop: 0})
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ['Data Analyst', 'Software Engineer', 'ML Enthusiast'],
+        typeSpeed: 100,
+        backSpeed: 70,
+        loop: true,
+        showCursor: false
+    })
+});
